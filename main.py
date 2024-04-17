e0d6994af2621eb857ccc4006f57eed8eddfad50
Alexei Klimovich <prostolex2004@mail.ru>
2024-04-17 11:23:19 +0300

    Changed on YOLOv8 model\nUpdated web

--- main.py
+++ main.py
@@ -1,28 +1,16 @@
 import io
+import cv2
 import streamlit as st
 from PIL import Image
-import numpy as np
-
-from keras.applications import EfficientNetB0
-from keras.preprocessing import image
-from keras.applications.efficientnet import preprocess_input, decode_predictions
-
+from ultralytics import YOLO
 
 def load_model():
-    model = EfficientNetB0(weights="imagenet")
+    model = YOLO('./best.pt')
     return model
 
 
-def preprocess_image(img):
-    img = img.resize((224, 224))
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
-    return x
-
-
 def load_image():
-    uploaded_file = st.file_uploader(label="Выберите изображение для распознования")
+    uploaded_file = st.file_uploader(label="Выберите изображение для детекции")
     if uploaded_file is not None:
         image_data = uploaded_file.getvalue()
         st.image(image_data)
@@ -31,21 +19,23 @@ def load_image():
         return None
     
 
-def print_predictions(preds):
-    classes = decode_predictions(preds, top=3)[0]
-    for cl in classes:
-        st.write(cl[1], ": " + str(round(100*cl[2], 2)) + "%")
+def detect_objects():
+    results = model(img, imgsz=640, iou=0.4, conf=0.4, verbose=True)
+    annotated_frame = results[0].plot()
+    annotated_frame = cv2.cvtColor(annotated_frame, cv2.COLOR_BGR2RGB)
+    st.image(annotated_frame)
 
-    
 
 model = load_model()
 
 st.set_page_config(page_title="SkyVision")
-st.title("Классификация изображений")
+st.markdown("<h1 style='text-align: center;'>SkyVision</h1>", unsafe_allow_html=True)
+st.write("<h3 style='text-align: center;'>Детекция объектов на спутниковых снимках земной поверхности</h3>", unsafe_allow_html=True)
+
 img = load_image()
-result = st.button("Распознать изображение")
+
+result = st.button("Запустить детекцию")
+
 if img and result:
-    x = preprocess_image(img)
-    preds = model.predict(x)
-    st.write("**Результаты распознования:**")
-    print_predictions(preds)
+    st.write("### **Результаты детекции:**")
+    detect_objects()
